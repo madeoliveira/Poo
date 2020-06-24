069c2e668ea7082c043f5813708a3385fb443004
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-06-24 18:29:50 -0300

    Commit inicial

---
+++ PokemonApp/PokemonApp/Pokemon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonApp
+{
+    public class Pokemon
+    {
+        public Pokemon()
+        {
+            this.Nome = "";
+            this.Descricao = "";
+        }
+
+        private String nome;
+
+        public String Nome
+        {
+            get {
+                return nome; 
+            }
+            set {
+                String texto = value;
+                nome = texto;
+            }
+        }
+        private String descricao;
+
+        public String Descricao
+        {
+            get { 
+                return this.descricao; 
+            }
+            set {
+                this.descricao = value; 
+            }
+        }
+
+        public void ExibirDadosPokemon()
+        {
+            Console.WriteLine("Nome do Pokémon: " + this.Nome);
+            Console.WriteLine("Descrição do Pokémon: " + this.Descricao);
+        }
+
+    }
+}
---
+++ PokemonApp/PokemonApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon pokemon = new Pokemon();
+            pokemon.Nome = "Bulbasaouro";
+            pokemon.Descricao = "Bulbasauro é um Pokemon tipo planta ";
+            pokemon.ExibirDadosPokemon();
+            Console.ReadKey();
+        }
+    }
+}
