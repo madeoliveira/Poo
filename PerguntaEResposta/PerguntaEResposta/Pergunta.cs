782ca07db799e208aefb9fee7094fadee2ea3936
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-06-24 20:02:47 -0300

    Propriedades simplificadas

---
+++ PerguntaEResposta/PerguntaEResposta/Pergunta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerguntaEResposta
+{
+    class Pergunta
+    {
+        public Pergunta()
+        {
+            this.Texto = "";
+            this.Resposta = "";
+
+        }
+
+        public Pergunta(String testo,String resposta)
+        {
+            this.Texto = texto;
+            this.Resposta = resposta;
+
+        }
+        private String texto;
+
+        public String Texto
+        {
+            get { return texto; }
+            set { texto = value; }
+        }
+        public String Resposta { get; set; }
+    }
+}
