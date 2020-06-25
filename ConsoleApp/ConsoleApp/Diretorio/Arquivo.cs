3300dea4db2dd5833e03aaa5f34736a3931a0e1b
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-06-25 15:43:29 -0300

    Exemplos

---
+++ ConsoleApp/ConsoleApp/Diretorio/Arquivo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Arquivos
+{
+    class Arquivo
+    {
+        public static void Ler(int numeroArquivo)
+        {
+            string arquivoComCaminho = @"C:\arquivos\arq" + numeroArquivo + ".txt";
+            Console.WriteLine("============ Lendo arquivo ===========\n" + arquivoComCaminho+ "\n======================" );
+                if (File.Exists(arquivoComCaminho))
+                {
+                using(StreamReader arquivo = File.OpenText(arquivoComCaminho))
+                    {
+                    string linha;
+                    while((linha = arquivo.ReadLine()) != null)
+                        {
+                        Console.WriteLine(linha);
+                         }
+
+                 }
+            }
+            string arquivoComCaminho2 = @"C:\arquivos\arq" + (numeroArquivo + 1) + ".txt";
+            if (File.Exists(arquivoComCaminho2))
+            {
+                Ler(numeroArquivo + 1);
+            }
+            Console.ReadKey();
+        }
+       
+        
+       
+    }
+   
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp.Arquivos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Media.Aluno();
+            Arquivo.Ler(1);
+        }
+    }
+}
